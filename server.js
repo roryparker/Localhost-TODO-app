a9d729c945dbfc413c52f56eec35599f33af6676
roryparker <rorysethparker@protonmail.com>
2021-05-03 23:12:03 -0400

    removed express-flash

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
-const flash = require('express-flash')
+// const flash = require('express-flash')
 const logger = require('morgan')
 const connectDB = require('./config/database')
 const mainRoutes = require('./routes/main')
@@ -36,7 +36,7 @@ app.use(
 app.use(passport.initialize())
 app.use(passport.session())
 
-app.use(flash())
+// app.use(flash())
   
 app.use('/', mainRoutes)
 app.use('/todos', todoRoutes)
